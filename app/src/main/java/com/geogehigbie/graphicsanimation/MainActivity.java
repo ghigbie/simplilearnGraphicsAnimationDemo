2fbb346a91b1dccde0a0d4368daebe38883c0157
George Higbie <gehigbie@gmail.com>
2016-11-06 19:54:43 -0500

    demo code for students to use

--- app/src/main/java/com/geogehigbie/graphicsanimation/MainActivity.java
+++ app/src/main/java/com/geogehigbie/graphicsanimation/MainActivity.java
@@ -1,13 +1,49 @@
 package com.geogehigbie.graphicsanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView kitten;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kitten = (ImageView) findViewById(R.id.kitten);
+
+    }
+
+
+    public void onClickRotate(View view){
+
+        kitten.animate().rotation(720).setDuration(1500).start();
+
+    }
+
+    public void onClickTrans(View view){
+
+        kitten.animate().translationY(700).setDuration(200).start();
+
+    }
+
+    public void onClickScale(View view){
+
+        kitten.animate().scaleX(0.5f).setDuration(1500).start();
+        kitten.animate().scaleY(0.5f).setDuration(1500).start();
+
+
+    }
+
+    public void onClickAlpha(View view){
+
+        kitten.animate().alpha(0.5f).setDuration(3000).start();
+
     }
 }
